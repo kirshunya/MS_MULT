277c2112fa0e1d7e7aac66b13a096bee1d95a476
kirshunya <bakunovich.nikita@mail.ru>
2024-12-25 22:21:47 +0300

    fix constructors.

--- Server/PlayerData/playerData.js
+++ Server/PlayerData/playerData.js
@@ -1,9 +1,9 @@
-// Класс для хранения данных игрока
-class PlayerData {
-    constructor(login, password, position = { x: 0, y: 0, z: 0 }, rotation = { x: 0, y: 0, z: 0 }) {
-        this.login = login; // Логин игрока
-        this.password = password; // Пароль игрока
-        this.position = position; // Позиция игрока
-        this.rotation = rotation; // Поворот игрока
-    }
-}
+// // Класс для хранения данных игрока
+// class PlayerData {
+//     constructor(login, password, position = { x: 0, y: 0, z: 0 }, rotation = { x: 0, y: 0, z: 0 }) {
+//         this.login = login; // Логин игрока
+//         this.password = password; // Пароль игрока
+//         this.position = position; // Позиция игрока
+//         this.rotation = rotation; // Поворот игрока
+//     }
+// }
--- Server/server.js
+++ Server/server.js
@@ -13,6 +13,16 @@ const server = http.createServer(app);
 // Настройка CORS для всех источников
 app.use(cors());
 
+// Класс для хранения данных игрока
+class PlayerData {
+    constructor(login, password, position = { x: 0, y: 0, z: 0 }, rotation = { x: 0, y: 0, z: 0 }) {
+        this.login = login; // Логин игрока
+        this.password = password; // Пароль игрока
+        this.position = position; // Позиция игрока
+        this.rotation = rotation; // Поворот игрока
+    }
+}
+
 // Получаем адрес и порт из переменных окружения
 const PORT = process.env.PORT || 8080;
 
